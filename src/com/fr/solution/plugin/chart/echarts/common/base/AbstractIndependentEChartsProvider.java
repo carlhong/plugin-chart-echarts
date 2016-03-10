49cd04250194036507c1aafcdc61ddaac97227e6
richie <richie@finereport.com>
2016-03-10 10:36:36 +0800

    插件中添加API_LEVEL

--- src/com/fr/solution/plugin/chart/echarts/common/base/AbstractIndependentEChartsProvider.java
+++ src/com/fr/solution/plugin/chart/echarts/common/base/AbstractIndependentEChartsProvider.java
@@ -1,11 +1,11 @@
 package com.fr.solution.plugin.chart.echarts.common.base;
 
-import com.fr.chart.fun.impl.AbstractIndependentChartProvider;
+import com.fr.chart.fun.impl.AbstractIndependentChartProviderWithAPILevel;
 
 /**
  * Created by richie on 16/1/29.
  */
-public abstract class AbstractIndependentEChartsProvider extends AbstractIndependentChartProvider {
+public abstract class AbstractIndependentEChartsProvider extends AbstractIndependentChartProviderWithAPILevel {
 
     @Override
     public String[] getRequiredJS() {
